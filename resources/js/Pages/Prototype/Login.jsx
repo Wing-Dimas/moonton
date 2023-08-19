9bf1146ed3c6c36933a11edda410989db6373700
Wing-Dimas <wingdimas@gmail.com>
2023-08-19 15:24:48 +0700

    feat: slicing sign in and sign up

---
+++ resources/js/Pages/Prototype/Login.jsx
@@ -0,0 +1,96 @@
+import InputLabel from "@/Components/InputLabel";
+import TextInput from "@/Components/TextInput";
+import React from "react";
+import { Head, Link } from "@inertiajs/react";
+import Button from "@/Components/Button";
+
+export default function Login() {
+    return (
+        <>
+            <Head title="Sign In" />
+            <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
+                <div className="fixed top-[-50px] hidden lg:block">
+                    <img
+                        src="/images/signup-image.png"
+                        className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]"
+                        alt=""
+                    />
+                </div>
+                <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+                    <div>
+                        <img src="/images/moonton-white.svg" alt="" />
+                        <div className="my-[70px]">
+                            <div className="font-semibold text-[26px] mb-3">
+                                Welcome Back
+                            </div>
+                            <p className="text-base text-[#767676] leading-7">
+                                Explore our new movies and get <br />
+                                the better insight for your life
+                            </p>
+                        </div>
+                        <form className="w-[370px]">
+                            <div className="flex flex-col gap-6">
+                                <div>
+                                    <InputLabel forInput="input">
+                                        Email Address
+                                    </InputLabel>
+                                    <TextInput
+                                        type="email"
+                                        name="email"
+                                        placeholder="Email Address"
+                                    />
+                                </div>
+                                <div>
+                                    <InputLabel forInput="password">
+                                        Password
+                                    </InputLabel>
+                                    <TextInput
+                                        type="password"
+                                        name="password"
+                                        placeholder="Password"
+                                    />
+                                </div>
+                            </div>
+                            <div className="grid space-y-[14px] mt-[30px]">
+                                {/* <a
+                                href="/"
+                                className="rounded-2xl bg-alerange py-[13px] text-center"
+                            >
+                                <span className="text-base font-semibold">
+                                    Start Watching
+                                </span>
+                            </a> */}
+                                <Button type="button" variant="primary">
+                                    <span className="text-base font-semibold">
+                                        Start Watching
+                                    </span>
+                                </Button>
+                                {/* <a
+                                href="sign_up.html"
+                                className="rounded-2xl border border-white py-[13px] text-center"
+                            >
+                                <span className="text-base text-white">
+                                    Create New Account
+                                </span>
+                            </a> */}
+                                <Link
+                                    href={route("prototype.register")}
+                                    className="w-full"
+                                >
+                                    <Button
+                                        type="button"
+                                        variant="light-outline"
+                                    >
+                                        <span className="text-base text-white">
+                                            Create New Account
+                                        </span>
+                                    </Button>
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
